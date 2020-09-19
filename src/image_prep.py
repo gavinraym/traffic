263fd7bc711f7cfc3c0022ae8c86c559cb2ab283
Gavin Ray <gavinraym@gmail.com>
2020-09-19 09:24:57 -0700

    Updating image prep and adding 1 model

--- src/image_prep.py
+++ src/image_prep.py
@@ -18,19 +18,19 @@ enhance_list = [
                 ]
 
 
-for pil_name, pil_filter in filters_list:
-    os.mkdir(f'data/{pil_name}/')
-# There are 43 classes of signs. Here I am iterating over each class
-# and saving new copies of each picture into the processed images folder.
-    src_folder = os.listdir('data/train/')
-    for k in src_folder:
-        os.mkdir(f'data/{pil_name}/{k}/')
-        src_file = os.listdir(f'data/train/{k}')
+# for pil_name, pil_filter in filters_list:
+#     os.mkdir(f'data/{pil_name}/')
+# # There are 43 classes of signs. Here I am iterating over each class
+# # and saving new copies of each picture into the processed images folder.
+#     src_folder = os.listdir('data/train/')
+#     for k in src_folder:
+#         os.mkdir(f'data/{pil_name}/{k}/')
+#         src_file = os.listdir(f'data/train/{k}')
     
-        for img_file in src_file:
-            if img_file[-3:] == 'ppm':
-                image = Image.open(f'data/train/{k}/{img_file}')
-                image.filter(pil_filter).save(f'data/{pil_name}/{k}/{img_file}', 'ppm')
+#         for img_file in src_file:
+#             if img_file[-3:] == 'ppm':
+#                 image = Image.open(f'data/train/{k}/{img_file}')
+#                 image.filter(pil_filter).save(f'data/{pil_name}/{k}/{img_file}', 'ppm')
 
 for pil_name, pil_enhancer in enhance_list:
     os.mkdir(f'data/{pil_name}/')
@@ -45,12 +45,15 @@ for pil_name, pil_enhancer in enhance_list:
             if img_file[-3:] == 'ppm':
                 image = Image.open(f'data/train/{k}/{img_file}')
                 enhancer = pil_enhancer(image)
-                enhancer.enhance(.25).save(f'data/{pil_name}/{k}/{img_file}.ppm')
+                enhancer.enhance(.25).save(f'data/{pil_name}/{k}/{img_file}')
 
 src_folder = os.listdir('data/test/')
+os.mkdir('app/static/test_images/')
 for k in src_folder:
-    os.mkdir('app/static/test/')
-
+    
     for img_file in src_folder:
         if img_file[-3:] == 'ppm':
-            Image.open(f'data/test/{img_file}').save(f'app/static/test/{img_file[:-3]}.png')
+            Image.open(f'data/test/{img_file}').save(f'app/static/test_images/{img_file[:-3]}png')
+
+if __name__ == "__main__":
+    pass
